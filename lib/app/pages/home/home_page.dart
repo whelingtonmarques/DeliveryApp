7252136de29e8d797e4fd9eeada92b351279d4f7
whelingtonmarques <whelingtonmarquesfp@hotmsil.com>
2023-02-24 11:06:37 -0300

    widget dos produtos criado

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:delivery/app/core/ui/widgets/delivery_appbar.dart';
+import 'package:delivery/app/pages/home/widgets/delivery_product_tile.dart';
 import 'package:flutter/material.dart';
+import '../../models/product_model.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -6,10 +9,25 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
-      body: Container(),
+      appBar: DeliveryAppbar(),
+      body: Column(children: [
+        Expanded(
+            child: ListView.builder(
+          itemCount: 5,
+          itemBuilder: (context, index) {
+            return DeliveryProductTile(
+              product: ProductModel(
+                id: 0,
+                name: 'cu',
+                description: 'cabeludo',
+                price: 10.0,
+                image:
+                    'https://assets.unileversolutions.com/recipes-v2/106684.jpg?imwidth=800',
+              ),
+            );
+          },
+        ))
+      ]),
     );
   }
 }
